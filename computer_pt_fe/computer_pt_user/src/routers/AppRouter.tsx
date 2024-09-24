21d7bc6be31b66099751ed042ee2028e864f692d
dangvietquang59 <dvquang5902@gmail.com>
2024-09-24 19:42:33 +0700

    feat: done ui details

--- computer_pt_fe/computer_pt_user/src/routers/AppRouter.tsx
+++ computer_pt_fe/computer_pt_user/src/routers/AppRouter.tsx
@@ -3,6 +3,7 @@ import NotFound from "@/pages/NotFound";
 import Home from "@/pages/Home";
 import paths from "@/utils/constants/paths";
 import RootLayout from "@/layouts/RootLayout";
+import ProductDetails from "@/pages/ProductDetails";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
       {
         path: paths.HOME,
         element: <Home />,
+      },
+      {
+        path: `${paths.PRODUCTS}/:slug`,
+        element: <ProductDetails />,
       },
     ],
   },
